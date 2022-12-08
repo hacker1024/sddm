9064e7e32aed8f5c54be69c7ca21d27cb966dac6
hacker1024 <hacker1024@users.sourceforge.net>
2022-12-08 23:17:43 +1100

    Add a system-wide default session configuration option
    Based on https://github.com/NixOS/nixpkgs/blob/91812b84b8b2ec4051c23bb11d7eb92eba367780/pkgs/applications/display-managers/sddm/sddm-default-session.patch.

--- src/common/Configuration.h
+++ src/common/Configuration.h
@@ -47,6 +47,7 @@ namespace SDDM {
                                                                                                    "NOTE: Currently ignored if autologin is enabled."));
         Entry(InputMethod,         QString,     QStringLiteral("qtvirtualkeyboard"),                   _S("Input method module"));
         Entry(Namespaces,          QStringList, QStringList(),                                  _S("Comma-separated list of Linux namespaces for user session to enter"));
+        Entry(DefaultSession,      QString,     QString(),                                      _S("System-wide default session"));
         Entry(GreeterEnvironment,  QStringList, QStringList(),                                  _S("Comma-separated list of environment variables to be set"));
         //  Name   Entries (but it's a regular class again)
         Section(Theme,
